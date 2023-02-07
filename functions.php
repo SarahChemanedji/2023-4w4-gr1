3179c44dccd456afeb1ea693979588e667783367
Eddy Martin <eddypmartin@gmail.com>
2023-02-07 11:10:32 -0500

    s3c1 configuration du menu

--- functions.php
+++ functions.php
@@ -10,4 +10,13 @@ wp_enqueue_style('4w4-gr1-principal', // id
         'all'); // media
 }        
 
-add_action( 'wp_enqueue_scripts', 'enfiler_css' );        
+add_action( 'wp_enqueue_scripts', 'enfiler_css' );  
+
+/* -------------------------------------- Enregitrement des menus */
+function enregistre_menus(){
+        register_nav_menus( array(
+        'menu_entete' => 'Menu entete',
+        'menu_sidebar'  => 'Menu sidebar',
+        ) );
+}
+add_action( 'after_setup_theme', 'enregistre_menus', 0 );
